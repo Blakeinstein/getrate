c90890597ec8095e4afb7f8d667dd8dec262c2b5
Blaine <rishi1998@gmail.com>
2021-04-12 12:46:02 +0530

    make compilable build

--- docker/src/lib.rs
+++ docker/src/lib.rs
@@ -1,6 +1,6 @@
 use rillrate::Table;
 use anyhow::Error;
-use meio::{Actor, Context, InterruptedBy, StartedBy, System, task::*};
+use meio::{Actor, Context, InterruptedBy, StartedBy, System};
 use async_trait::async_trait;
 use std::time::SystemTime;
 use shiplift::Docker;
@@ -12,9 +12,9 @@ struct DockerWatcher{
 }
 
 impl DockerWatcher {
-    pub fn new() -> Result<Self, Box<dyn error::Error>> {
+    pub fn new() -> Result<Self, Error> {
         let docker = Docker::new();
-        let docker_table = Table::create("Containers");
+        let docker_table = Table::create("Containers")?;
         docker_table.add_col(0.into(), Some("created".into()));
         docker_table.add_col(1.into(), Some("command".into()));
         docker_table.add_col(2.into(), Some("id".into()));
@@ -34,15 +34,39 @@ impl DockerWatcher {
     fn set_info(&self, container: &Container, id: u64) {
         let row_id = id.into();
         let timestamp = Some(SystemTime::now());
+        let mut labels = container.labels.iter().fold(String::from(""), |acc, x| {
+            format!("{}{}: {}, ", acc, x.0, x.1)
+        });
+        let mut ports = container.ports.iter().fold(String::from(""), |acc, x| {
+            match &x.ip {
+                Some(ip) => {
+                    let val = if let Some(pp) = x.public_port {
+                        format!("{}:{}, {}", ip, x.private_port, pp)
+                    } else {
+                        format!("{}:{}", ip, x.private_port)
+                    };
+                    format!("{}{}, ", acc, val)
+                }
+                None => acc
+            }
+        });
+        if !ports.is_empty() {
+            ports.pop();
+            ports.pop();
+        }
+        if !labels.is_empty() {
+            labels.pop();
+            labels.pop();
+        }
         self.docker_table.set_cell(row_id, 0.into(), container.created.to_rfc2822(), timestamp);
-        self.docker_table.set_cell(row_id, 1.into(), container.command, timestamp);
-        self.docker_table.set_cell(row_id, 2.into(), container.id, timestamp);
-        self.docker_table.set_cell(row_id, 3.into(), container.image, timestamp);
-        self.docker_table.set_cell(row_id, 4.into(), container.labels, timestamp);
+        self.docker_table.set_cell(row_id, 1.into(), &container.command, timestamp);
+        self.docker_table.set_cell(row_id, 2.into(), &container.id, timestamp);
+        self.docker_table.set_cell(row_id, 3.into(), &container.image, timestamp);
+        self.docker_table.set_cell(row_id, 4.into(), labels, timestamp);
         self.docker_table.set_cell(row_id, 5.into(), container.names.join(","), timestamp);
-        self.docker_table.set_cell(row_id, 6.into(), container.ports, timestamp);
-        self.docker_table.set_cell(row_id, 7.into(), container.state, timestamp);
-        self.docker_table.set_cell(row_id, 8.into(), container.status, timestamp);
+        self.docker_table.set_cell(row_id, 6.into(), ports, timestamp);
+        self.docker_table.set_cell(row_id, 7.into(), &container.state, timestamp);
+        self.docker_table.set_cell(row_id, 8.into(), &container.status, timestamp);
         self.docker_table.set_cell(row_id, 9.into(), container.size_rw.unwrap_or(0).to_string(), timestamp);
     }
 }
@@ -54,8 +78,8 @@ impl Actor for DockerWatcher {
 #[async_trait]
 impl StartedBy<System> for DockerWatcher {
     async fn handle(&mut self, ctx: &mut Context<Self>) -> Result<(), Error> {
-        for (id, container) in self.docker.containers().list(&Default::default()).await?.enumerate() {
-            self.set_info(container, id);
+        for (id, container) in self.docker.containers().list(&Default::default()).await?.iter().enumerate() {
+            self.set_info(container, id as u64);
         };
         Ok(())
     }
@@ -70,8 +94,7 @@ impl InterruptedBy<System> for DockerWatcher {
 }
 
 #[tokio::main]
-pub async fn main() -> Result<(), Box<dyn error::Error>> {
-    env_logger::try_init()?;
+pub async fn main() -> Result<(), Error> {
     let docker = DockerWatcher::new()?;
     let docker_mon = System::spawn(docker);
     
--- getrate/src/main.rs
+++ getrate/src/main.rs
@@ -2,6 +2,7 @@ use proc;
 use docker;
 use std::error::Error;
 use rillrate::RillRate;
+use env_logger;
 
 fn main() -> Result<(), Box<dyn Error>>{
     env_logger::try_init()?;
